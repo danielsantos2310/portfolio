91980700f151d7ea0468f38e72b3846f1bb029b0
danielsantos2310 <danieldecooo@gmail.com>
2022-05-04 22:42:11 +0300

    routing bug fixed

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import Contact from './components/Contact'
 import Layout from './components/Layout'
 import CV from './components/CV/cv'
 import './App.scss'
+import { Expense } from './components/Expense'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cv" element={<CV />} />
-          <Route path="*" element={<div>Resource Not found</div>} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<CV />} />
+        <Route path="/expense" element={<Expense />} />
+        <Route path="*" element={<div>No content</div>} />
+      </Route>
+    </Routes>
+  </>
   )
 }
 
---
+++ src/components/Expense/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Expense = () => {
+  return (
+    <div>Hello expense</div>
+  )
+}
+
